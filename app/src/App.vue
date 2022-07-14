9f6984117b82144625ab1facef445be945ff500c
ChadBrow <68403385+ChadBrow@users.noreply.github.com>
2022-07-13 20:34:46 -0400

    Fixed error with MapData being undefined

--- app/src/App.vue
+++ app/src/App.vue
@@ -13,11 +13,11 @@ export default {
 
   data: () => ({
     maps: [ // these are place-holders, will need to change them to jsons for the actual thing
-      new MapData("Example 1", 0, null, "http://127.0.0.1:5500/Potree/potree-1.8/examples/cesium_ca13.html"),
-      new MapData("Example 2", 1, null, "http://127.0.0.1:5500/Potree/potree-1.8/examples/cesium_ca13.html"),
-      new MapData("Example 3", 2, null, "http://127.0.0.1:5500/Potree/potree-1.8/examples/cesium_sorvilier.html"),
-      new MapData("Example 4", 3, null, null),
-      new MapData("Example 5", 4, null, null)
+      // new MapData("Example 1", 0, null, "http://127.0.0.1:5500/Potree/potree-1.8/examples/cesium_ca13.html"),
+      // new MapData("Example 2", 1, null, "http://127.0.0.1:5500/Potree/potree-1.8/examples/cesium_ca13.html"),
+      // new MapData("Example 3", 2, null, "http://127.0.0.1:5500/Potree/potree-1.8/examples/cesium_sorvilier.html"),
+      // new MapData("Example 4", 3, null, null),
+      // new MapData("Example 5", 4, null, null)
     ],
     currMapIndex: -1
   }),
