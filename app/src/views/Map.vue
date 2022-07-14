7ce1f455179ad6c7ec22c9e0e35d8b3329049c88
Chase Brown <chasebrow@outlook.com>
2022-07-14 12:25:05 -0700

    save

--- app/src/views/Map.vue
+++ app/src/views/Map.vue
@@ -6,41 +6,41 @@
 
 <script>   
 //import libraries
-import THREE from 'three';
-import * as Potree from '../potree/Potree.js'
+// import THREE from 'three';
 
 export default{
 
     mounted(){
-        console.log(document.getElementById("potree_render_area"));
+        console.log(window)
+        // console.log(document.getElementById("potree_render_area"));
 
-        //Initialize Potree viewer
-        window.viewer = new Potree.Viewer(document.getElementById("potree_render_area"));
+        // //Initialize Potree viewer
+        // window.viewer = new Potree.Viewer(document.getElementById("potree_render_area"));
 
-        //Configure viewer settings
-        window.viewer.setEDLEnabled(true);
-        window.viewer.setFOV(60);
-        window.viewer.setPointBudget(1_000_000);
-        window.viewer.loadSettingsFromURL();
+        // //Configure viewer settings
+        // window.viewer.setEDLEnabled(true);
+        // window.viewer.setFOV(60);
+        // window.viewer.setPointBudget(1_000_000);
+        // window.viewer.loadSettingsFromURL();
 
-        window.viewer.setDescription("");
+        // window.viewer.setDescription("");
 
-        window.viewer.loadGUI(() => {
-        window.viewer.setLanguage('en');
-        window.$("#menu_appearance").next().show();
-            //viewer.toggleSidebar();   
-        });
+        // window.viewer.loadGUI(() => {
+        // window.viewer.setLanguage('en');
+        // window.$("#menu_appearance").next().show();
+        //     //viewer.toggleSidebar();   
+        // });
 
-        // Load pointcloud
-        Potree.loadPointCloud("./assets/pointclouds/lion_takanawa/cloud.js", "lion", function(e){
-            viewer.scene.addPointCloud(e.pointcloud);
+        // // Load pointcloud
+        // Potree.loadPointCloud("./assets/pointclouds/lion_takanawa/cloud.js", "lion", function(e){
+        //     viewer.scene.addPointCloud(e.pointcloud);
 
-            let material = e.pointcloud.material;
-            material.size = 1;
-            material.pointSizeType = Potree.PointSizeType.ADAPTIVE;
+        //     let material = e.pointcloud.material;
+        //     material.size = 1;
+        //     material.pointSizeType = Potree.PointSizeType.ADAPTIVE;
 
-            window.viewer.fitToScreen();
-        });
+        //     window.viewer.fitToScreen();
+        // });
     }
 }
 
