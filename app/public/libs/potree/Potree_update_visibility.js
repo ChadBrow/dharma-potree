a049761aafcf924e22540c63ff6a01825f74699d
Chase Brown <chasebrow@outlook.com>
2022-07-14 13:06:20 -0700

    master

--- app/public/libs/potree/Potree_update_visibility.js
+++ app/public/libs/potree/Potree_update_visibility.js
@@ -3,7 +3,7 @@ import * as THREE from "../three.js/build/three.module.js";
 import * as Potree from "./Potree.js";
 import {ClipTask, ClipMethod} from "./defines.js";
 import {Box3Helper} from "./utils/Box3Helper.js";
-import {PriorityQueue} from "../other/BinaryHeap.js";
+import {BinaryHeap, PriorityQueue} from "../other/BinaryHeap.js";
 
 export function updatePointClouds(pointclouds, camera, renderer){
 
@@ -39,7 +39,7 @@ export function updatePointClouds(pointclouds, camera, renderer){
 export function updateVisibilityStructures(pointclouds, camera, renderer) {
 	let frustums = [];
 	let camObjPositions = [];
-	let priorityQueue = new PriorityQueue();
+	let priorityQueue = new BinaryHeap();
 
 	for (let i = 0; i < pointclouds.length; i++) {
 		let pointcloud = pointclouds[i];
--- app/src/views/Map.vue
+++ app/src/views/Map.vue
@@ -1,7 +1,5 @@
 <template>
-    <v-container class="potree_container" style="position: absolute; width: 100%; height: 100%; left: 0px; top: 0px; ">
-		<div id="potree_render_area" style="background-image: '../build/potree/resources/images/background.jpg';"></div>
-	</v-container>
+    <div id="potree_render_area" style="background-image: '../build/potree/resources/images/background.jpg';"></div>
 </template>
 
 <script>   
@@ -17,33 +15,33 @@ export default{
         console.log(window)
         console.log(document.getElementById("potree_render_area"));
 
-        // //Initialize Potree viewer
-        // window.viewer = new Potree.Viewer(document.getElementById("potree_render_area"));
+        //Initialize Potree viewer
+        window.viewer = new Potree.Viewer(document.getElementById("potree_render_area"));
 
-        // //Configure viewer settings
+        //Configure viewer settings
         // window.viewer.setEDLEnabled(true);
-        // window.viewer.setFOV(60);
-        // window.viewer.setPointBudget(1_000_000);
-        // window.viewer.loadSettingsFromURL();
+        window.viewer.setFOV(60);
+        window.viewer.setPointBudget(1_000_000);
+        window.viewer.loadSettingsFromURL();
 
-        // window.viewer.setDescription("");
+        window.viewer.setDescription("");
 
-        // window.viewer.loadGUI(() => {
-        // window.viewer.setLanguage('en');
-        // window.$("#menu_appearance").next().show();
-        //     //viewer.toggleSidebar();   
-        // });
+        window.viewer.loadGUI(() => {
+        window.viewer.setLanguage('en');
+        window.$("#menu_appearance").next().show();
+            viewer.toggleSidebar();   
+        });
 
-        // // Load pointcloud
-        // Potree.loadPointCloud("./assets/pointclouds/lion_takanawa/cloud.js", "lion", function(e){
-        //     viewer.scene.addPointCloud(e.pointcloud);
+        // Load pointcloud
+        Potree.loadPointCloud("./assets/pointclouds/lion_takanawa/cloud.js", "lion", function(e){
+            viewer.scene.addPointCloud(e.pointcloud);
 
-        //     let material = e.pointcloud.material;
-        //     material.size = 1;
-        //     material.pointSizeType = Potree.PointSizeType.ADAPTIVE;
+            let material = e.pointcloud.material;
+            material.size = 1;
+            material.pointSizeType = Potree.PointSizeType.ADAPTIVE;
 
-        //     window.viewer.fitToScreen();
-        // });
+            window.viewer.fitToScreen();
+        });
     }
 }
 
