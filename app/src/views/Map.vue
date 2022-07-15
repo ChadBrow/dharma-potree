e42e8b4556df42668374132bea894e10b989b480
ChadBrow <68403385+ChadBrow@users.noreply.github.com>
2022-07-15 18:54:40 -0400

    Got the mouse intersection to display in console

--- app/src/views/Map.vue
+++ app/src/views/Map.vue
@@ -1,7 +1,9 @@
 <template>
     <div id="potree_container" style="position: absolute; width: 100%; height: 100%; left: 0px; top: 0px; ">
         <v-container style="height: 5%; padding: 0">
-            <v-btn>Test</v-btn>
+            <v-btn 
+                v-on:click="()=>{showIntersectionOnClick = !showIntersectionOnClick}" >
+                Show Intersection on Click</v-btn>
         </v-container>
         <div id="potree_render_area" style="height: 95%; width: 100%; background-image: '../build/potree/resources/images/background.jpg';">
         </div>
@@ -13,6 +15,12 @@
 // import THREE from 'three';
 
 export default{
+    data(){
+        return {
+            showIntersectionOnClick: false,
+            mouseLoc: null
+        }
+    },
 
     mounted(){
         const Potree = window.Potree
@@ -21,8 +29,9 @@ export default{
         console.log(window)
         console.log(document.getElementById("potree_render_area"));
 
-        //Initialize Potree viewer
+        //Initialize Potree viewer and scene
         window.viewer = new Potree.Viewer(document.getElementById("potree_render_area"));
+        let scene = window.viewer.scene;
 
         //Configure viewer settings
         // window.viewer.setEDLEnabled(true);
@@ -42,7 +51,6 @@ export default{
         Potree.loadPointCloud("./assets/pointclouds/lion_takanawa/cloud.js", "lion", function(e){
             //Initialize some important variable
             let pointcloud = e.pointcloud;
-			let scene = window.viewer.scene;
 			let material = pointcloud.material;
 
             //Add pointcloud to seen
@@ -53,6 +61,17 @@ export default{
             material.pointSizeType = Potree.PointSizeType.ADAPTIVE;
 
             window.viewer.fitToScreen();
+        });
+
+        //Add event listner for mouse movement. This allows us to get pointcloud intersection with mouse
+        window.viewer.renderer.domElement.addEventListener('mousedown', (event) => {
+            if (this.showIntersectionOnClick){
+                let mouseLoc = window.viewer.inputHandler.mouse;
+                let camera = scene.getActiveCamera();
+
+                let hit = Potree.Utils.getMousePointCloudIntersection(mouseLoc, camera, viewer, scene.pointclouds);
+                console.log(hit);
+            }
         });
     }
 }
